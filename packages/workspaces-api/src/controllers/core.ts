a238e718bf213d815cc06fab5a0dc97bb5a0328a
Kalin Kostov <k.kostov.eu@gmail.com>
2020-07-16 11:35:36 +0300

    fixed new frame of undefined
    Signed-off-by: Kalin Kostov <k.kostov.eu@gmail.com>

--- packages/workspaces-api/src/controllers/core.ts
+++ packages/workspaces-api/src/controllers/core.ts
@@ -54,7 +54,7 @@ export class CoreController implements WorkspacesController {
             throw new Error(`This layout: ${name} cannot be restored, because it doesn't exist.`);
         }
 
-        const frameInstance: Instance = await this.frameUtils.getFrameInstance({ frameId: options?.frameId, newFrame: options.newFrame });
+        const frameInstance: Instance = await this.frameUtils.getFrameInstance({ frameId: options?.frameId, newFrame: options?.newFrame });
 
         return await this.base.restoreWorkspace(name, options, frameInstance);
     }
